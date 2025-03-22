ed9821ff62851910c3b887fc3a32bd038bcc21fb
Gunawardane K.C. <kavinduchanditha@gmail.com>
2025-03-22 20:05:25 +0530

    error update

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -17,7 +17,7 @@ const Card = async ({ event, hasOrderLink, hidePrice }: CardProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-  const isEventCreator = userId === event.organizer._id.toString();
+  const isEventCreator = event.organizer && userId === event.organizer._id.toString();
   
   const eventId = (event?._id as string) || '';
   const searchText = '';
